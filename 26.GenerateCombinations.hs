e089d04ab924b4e51260812538e2007c1f08486f
sienheiser <emmanueldcosta97@gmail.com>
2024-06-29 20:21:42 +0200

    Fixed error

--- 26.GenerateCombinations.hs
+++ 26.GenerateCombinations.hs
@@ -1,6 +1,5 @@
-
-comb :: Int -> [a] -> [[a]]
-comb _ [] = []
-comb 1 xs = [[x] | x <- xs]
-comb n (x:xs) = map (x:) (comb (n-1) xs) ++ comb n xs
+combinations :: Int -> [a] -> [[a]]
+combinations _ [] = []
+combinations 1 xs = [[x] | x <- xs]
+combinations n (x:xs) = map (x:) (combinations (n-1) xs) ++ combinations n xs
 
