a10be4764d6b43ae671edcea7131fe0ca153a507
emmanueldcosta <emmanuel.dcosta@ess.eu>
2024-07-01 15:29:03 +0200

    Trying different functions

--- testbench.hs
+++ testbench.hs
@@ -1,22 +1,5 @@
+f :: [a] -> [([a],[a])]
+f [x] = [([],[x])]
+f (x:xs) = [(x:ys,zs)|(ys,zs) <- f xs]
 
-
-f :: [a] -> [[[a]]]
-f xs = [[[x]] | x <- xs]
-
-
-
-combination :: Int -> [a] -> [([a],[a])]
-combination 0 xs     = [([],xs)]
-combination n []     = []
-combination n (x:xs) = ds ++ ts 
-  where
-    ts = [ (x:ys,zs) | (ys,zs) <- combination (n-1) xs ]
-    ds = [ (ys,x:zs) | (ys,zs) <- combination  n    xs ]
-
-comb :: Int -> [a] -> [([a],[a])]
-comb 0 xs     = [([],xs)]
-comb n []     = []
-comb n (x:xs) = ts ++ ds
-  where
-    ts = [ (x:ys,zs) | (ys,zs) <- comb (n-1) xs ]
-    ds = [ (ys,x:zs) | (ys,zs) <- comb  n    xs ]
+g :: [a] -> [([a],[a])]
